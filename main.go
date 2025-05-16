5a31200b2d6503220e9d09844d4b647fd459b462
PietPadda <pietergdewaal@gmail.com>
2025-05-16 22:34:47 +0200

    add mux, server and listen with err handle

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	// std go libraries
+	"log"      // for err loggin
+	"net/http" // http protocol
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// create server mux for routing http requests
+	mux := http.NewServeMux()
+
+	// create Server struct for config
+	server := &http.Server{ //ptr is more efficient than new copy
+		Addr:    ":8080", //server listens to port 8080 for all incoming requests
+		Handler: mux,     // mux will "handle" our http request
+	}
+
+	// start the server
+	err := server.ListenAndServe() // "listens" to http requests on addr and let's mux handle them
+
+	// server start check
+	if err != nil {
+		// log the err and terminate server
+		log.Fatal(err)
+	}
 }
