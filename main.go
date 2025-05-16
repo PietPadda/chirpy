3070121186a177cb3e81fcf2dde635b4bbd96cb0
PietPadda <pietergdewaal@gmail.com>
2025-05-16 23:14:18 +0200

    added index.html, added a fileserver handler to our mux

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 	// create server mux for routing http requests
 	mux := http.NewServeMux()
 
+	// apply a fileserver handler to mux
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// http.Dir(".") -  tells server to run files "here"
+	// http.FileServer(...) - looks for the index.html
+	// mux.Handle("/", ...) -- server handle all requests
+
 	// create Server struct for config
 	server := &http.Server{ //ptr is more efficient than new copy
 		Addr:    ":8080", //server listens to port 8080 for all incoming requests
